fc6fc11b5f88172e381d4af65f1e3478f1ff30fd
zhabwe254 <zhabwe@gmail.com>
2024-07-02 10:49:48 +0300

    A

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,7 @@
+export default function cleanSet(set, startString) {
+  if (!startString || startString.length === 0) return '';
+  return Array.from(set)
+    .filter(value => value.startsWith(startString))
+    .map(value => value.slice(startString.length))
+    .join('-');
+}
